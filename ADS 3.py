0ea35868a9ae29ed5e88f1fb589693b0e32b7f25
franchaise <40702944+franchaise@users.noreply.github.com>
2023-01-19 11:51:58 +0000

    Add files via upload

---
+++ ADS 3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 11:46:15 2023
+
+@author: franc
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import describe
+
+def readfile(doc, columns, indicator):
+    filedata = pd.read_excel(doc, skiprows=3)
+    filedata = filedata.loc[filedata['Indicator Name'] == indicator]
+    filedata = filedata[columns]
+    filedata.set_index('Country Name', inplace  = True)
+    return filedata, filedata.T
+
+doc =r"C:\Users\franc\OneDrive\Desktop\WBDATA.xls"
+Indicator ='Electricity production from oil sources (% of total)'
+columns = ['Country Name', '1985', '2015']
+
+WB_DATA = readfile(doc, columns, Indicator)
+print(WB_DATA)
+x =WB_DATA[0]
+x_c= x.dropna()
+x_a =x_c.values
+print(x_a)
